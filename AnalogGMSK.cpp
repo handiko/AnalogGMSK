7d6d1584ae0c46f9fc2cdb3cb36d7ab1d008ce09
handiko <handikogesang@gmail.com>
2020-12-30 10:46:07 +0700

    Build & Implement: OK

--- AnalogGMSK.cpp
+++ AnalogGMSK.cpp
@@ -9,7 +9,183 @@
 
 AnalogGMSK::AnalogGMSK() {
 	// TODO Auto-generated constructor stub
+	UsingUART = NO_UART;
+	tick = 0;
+}
+/*
+ * Private methods
+ */
+void AnalogGMSK::initGPIOClock(void)
+{
+#if defined(GPIOA)
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+#endif
 
+#if defined(GPIOB)
+	__HAL_RCC_GPIOB_CLK_ENABLE();
+#endif
+
+#if defined(GPIOC)
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+#endif
+
+#if defined(GPIOD)
+	__HAL_RCC_GPIOD_CLK_ENABLE();
+#endif
+
+#if defined(GPIOE)
+	__HAL_RCC_GPIOE_CLK_ENABLE();
+#endif
+
+#if defined(GPIOF)
+	__HAL_RCC_GPIOF_CLK_ENABLE();
+#endif
+
+#if defined(GPIOG)
+	__HAL_RCC_GPIOG_CLK_ENABLE();
+#endif
+
+#if defined(GPIOH)
+	__HAL_RCC_GPIOH_CLK_ENABLE();
+#endif
+}
+
+void AnalogGMSK::initGPIOOutputLevel(void)
+{
+	peripherals.outputPorts.relayPort->BSRR = peripherals.outputPorts.relayPin << 16;
+}
+
+void AnalogGMSK::initConfigGPIOPinsOutput(void)
+{
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+	GPIO_InitStruct.Pin = peripherals.outputPorts.relayPin;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+#if defined(GPIO_SPEED_FREQ_VERY_HIGH)
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+#endif
+
+	HAL_GPIO_Init(peripherals.outputPorts.relayPort, &GPIO_InitStruct);
+}
+
+void AnalogGMSK::initConfigGPIOPinsInput(void)
+{
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+	GPIO_InitStruct.Pin = peripherals.inputPorts.bitsPin;
+	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+	GPIO_InitStruct.Pull = GPIO_PULLUP;
+	HAL_GPIO_Init(peripherals.inputPorts.bitsPort, &GPIO_InitStruct);
+
+	GPIO_InitStruct.Pin = peripherals.inputPorts.pttPin;
+	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+	GPIO_InitStruct.Pull = GPIO_PULLUP;
+	HAL_GPIO_Init(peripherals.inputPorts.pttPort, &GPIO_InitStruct);
+}
+
+void AnalogGMSK::initNVICInterrupt(void)
+{
+	HAL_NVIC_SetPriority(peripherals.interrupts.pttIRQ, 0, 0);
+	HAL_NVIC_EnableIRQ(peripherals.interrupts.pttIRQ);
+
+	HAL_NVIC_SetPriority(peripherals.interrupts.bitsIRQ, 0, 0);
+	HAL_NVIC_EnableIRQ(peripherals.interrupts.bitsIRQ);
+}
+
+void AnalogGMSK::init(void)
+{
+	initGPIOClock();
+	initGPIOOutputLevel();
+	initConfigGPIOPinsOutput();
+	initConfigGPIOPinsInput();
+	initNVICInterrupt();
+
+	HAL_TIM_Base_Start_IT(peripherals.htim);
+
+	HAL_DAC_Start(peripherals.hdac, DAC_CHANNEL_1);
+
+	if(UsingUART == USING_UART)
+	{
+
+	}
+}
+
+void AnalogGMSK::ModulateRisingEdge(void)
+{
+	HAL_DAC_SetValue(peripherals.hdac, DAC_CHANNEL_1, DAC_ALIGN_12B_R, gaussian[tick++]);
+	if(tick == SAMPLE_PER_SYMBOL)
+	{
+		pulseDir = PULSE_STOP;
+	}
+}
+
+void AnalogGMSK::ModulateFallingEdge(void)
+{
+	HAL_DAC_SetValue(peripherals.hdac, DAC_CHANNEL_1, DAC_ALIGN_12B_R, gaussian[--tick]);
+	if(tick == 0)
+	{
+		pulseDir = PULSE_STOP;
+	}
+}
+
+/*
+ * Public methods
+ */
+void AnalogGMSK::Start(InputPorts_t inputPorts, OutputPorts_t outputPorts, DAC_HandleTypeDef *hdac, TIM_HandleTypeDef *htim, IRQs_t interrupts)
+{
+	this->peripherals.inputPorts = inputPorts;
+	this->peripherals.outputPorts = outputPorts;
+	this->peripherals.htim = htim;
+	this->peripherals.hdac = hdac;
+	this->peripherals.interrupts = interrupts;
+
+	UsingUART = NO_UART;
+
+	init();
+}
+
+void AnalogGMSK::Start(InputPorts_t inputPorts, OutputPorts_t outputPorts, DAC_HandleTypeDef *hdac, TIM_HandleTypeDef *htim, IRQs_t interrupts, UART_HandleTypeDef *huart)
+{
+	this->peripherals.inputPorts = inputPorts;
+	this->peripherals.outputPorts = outputPorts;
+	this->peripherals.htim = htim;
+	this->peripherals.hdac = hdac;
+	this->peripherals.interrupts = interrupts;
+	this->peripherals.huart = huart;
+
+	UsingUART = USING_UART;
+
+	init();
+}
+
+void AnalogGMSK::Tick(void)
+{
+	if(pulseDir == PULSE_RISING_EDGE)
+	{
+		ModulateRisingEdge();
+	}
+	else if(pulseDir == PULSE_FALLING_EDGE)
+	{
+		ModulateFallingEdge();
+	}
+}
+
+void AnalogGMSK::SetPulseRising(void)
+{
+	peripherals.outputPorts.relayPort->BSRR = peripherals.outputPorts.relayPin;
+
+	pulseDir = PULSE_RISING_EDGE;
+	tick = 0;
+}
+
+void AnalogGMSK::SetPulseFalling(void)
+{
+	peripherals.outputPorts.relayPort->BSRR = peripherals.outputPorts.relayPin << 16;
+
+	pulseDir = PULSE_FALLING_EDGE;
+	tick = SAMPLE_PER_SYMBOL;
 }
 
 AnalogGMSK::~AnalogGMSK() {
--- AnalogGMSK.hpp
+++ AnalogGMSK.hpp
@@ -5,13 +5,106 @@
  *      Author: handiko
  */
 
-#ifndef SRC_ANALOGGMSK_HPP_
-#define SRC_ANALOGGMSK_HPP_
+#ifndef INC_ANALOGGMSK_HPP_
+#define INC_ANALOGGMSK_HPP_
+
+#include "main.h"
+
+#ifndef STM32F4
+#error "STM32F4xx must be used !"
+#else
+
+#define PULSE_RISING_EDGE 0
+#define PULSE_FALLING_EDGE 1
+#define PULSE_STOP 2
+
+#define SAMPLE_PER_SYMBOL 64
+
+#define NO_UART 0
+#define USING_UART 1
+
+struct InputPorts_t {
+	GPIO_TypeDef* bitsPort = BITS_GPIO_Port;
+	GPIO_TypeDef* pttPort = PTT_GPIO_Port;
+
+	uint16_t bitsPin = BITS_Pin;
+	uint16_t pttPin = PTT_Pin;
+};
+
+struct OutputPorts_t {
+	GPIO_TypeDef* relayPort = RELAY_GPIO_Port;
+
+	uint16_t relayPin = RELAY_Pin;
+};
+
+// TODO not implemented yet
+struct LedPorts_t {
+	GPIO_TypeDef* ledTxPort;
+	GPIO_TypeDef* ledDataPort;
+	GPIO_TypeDef* ledCHAPort;
+	GPIO_TypeDef* ledCHBPort;
+
+	uint16_t ledTxPin;
+	uint16_t ledDataPin;
+	uint16_t ledCHAPin;
+	uint16_t ledCHBPin;
+};
+
+struct IRQs_t {
+	IRQn_Type bitsIRQ = BITS_EXTI_IRQn;
+	IRQn_Type pttIRQ = PTT_EXTI_IRQn;
+};
 
 class AnalogGMSK {
+private:
+	uint8_t pulseDir = PULSE_RISING_EDGE;
+	uint16_t tick=0;
+	const uint32_t gaussian[SAMPLE_PER_SYMBOL] = {
+		19,41,64,91,119,151,186,223,
+		264,308,356,407,462,520,582,647,
+		717,790,866,946,1029,1115,1204,1295,
+		1389,1485,1583,1683,1784,1885,1988,2090,
+		2192,2294,2395,2494,2592,2689,2783,2874,
+		2963,3049,3132,3212,3288,3361,3430,3496,
+		3558,3616,3671,3722,3769,3814,3854,3892,
+		3927,3958,3987,4013,4037,4058,4078,4095
+	};
+
+	struct PeripheralTypeDef {
+		TIM_HandleTypeDef *htim;
+		DAC_HandleTypeDef *hdac;
+		UART_HandleTypeDef *huart;
+		InputPorts_t inputPorts;
+		OutputPorts_t outputPorts;
+		IRQs_t interrupts;
+	} peripherals;
+
+	bool UsingUART = NO_UART;
+
+	void initGPIOClock(void);
+	void initGPIOOutputLevel(void);
+	void initConfigGPIOPinsOutput(void);
+	void initConfigGPIOPinsInput(void);
+	void initNVICInterrupt(void);
+	void init(void);
+
+	void ModulateRisingEdge(void);
+	void ModulateFallingEdge(void);
+
 public:
 	AnalogGMSK();
+
+	void Start(InputPorts_t inputPorts, OutputPorts_t outputPorts, DAC_HandleTypeDef *hdac, TIM_HandleTypeDef *htim, IRQs_t interrupts);
+	void Start(InputPorts_t inputPorts, OutputPorts_t outputPorts, DAC_HandleTypeDef *hdac, TIM_HandleTypeDef *htim, IRQs_t interrupts, UART_HandleTypeDef *huart);
+
+	void Tick(void);
+
+	void SetPulseRising(void);
+	void SetPulseFalling(void);
+
 	virtual ~AnalogGMSK();
 };
+
+#endif
 
 #endif /* SRC_ANALOGGMSK_HPP_ */
