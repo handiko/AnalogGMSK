82312b4d872407b2da87d8d32a8078c6b3da930c
handiko <handikogesang@gmail.com>
2020-12-30 09:28:13 +0700

    initial commit

---
+++ AnalogGMSK.cpp
@@ -0,0 +1,18 @@
+/*
+ * AnalogGMSK.cpp
+ *
+ *  Created on: Dec 30, 2020
+ *      Author: handiko
+ */
+
+#include "AnalogGMSK.hpp"
+
+AnalogGMSK::AnalogGMSK() {
+	// TODO Auto-generated constructor stub
+
+}
+
+AnalogGMSK::~AnalogGMSK() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ AnalogGMSK.hpp
@@ -0,0 +1,17 @@
+/*
+ * AnalogGMSK.hpp
+ *
+ *  Created on: Dec 30, 2020
+ *      Author: handiko
+ */
+
+#ifndef SRC_ANALOGGMSK_HPP_
+#define SRC_ANALOGGMSK_HPP_
+
+class AnalogGMSK {
+public:
+	AnalogGMSK();
+	virtual ~AnalogGMSK();
+};
+
+#endif /* SRC_ANALOGGMSK_HPP_ */
